5a7d2c149566099f95bae46761c1e3b152edc028
Iliya Rudberg <iliya.rudberg@gmail.com>
2020-12-30 17:40:19 +0300

    chore: publish package to npm

--- .releaserc.js
+++ .releaserc.js
@@ -7,11 +7,8 @@ module.exports = {
 		// generate some nice release notes to use in our GitHub release
 		'@semantic-release/release-notes-generator',
 
-		// if it's a library/dockerfile, then bump the package.json version but don't publish to npm
-		'@semantic-release/npm',
-
-		// finally, push a new version as a release to GitHub with the changelog included
-		['@semantic-release/github', {
+		// finally, push a new version as a release to npm with the changelog included
+		['@semantic-release/npm', {
 			failComment: false, // don't create a 'release failed' issue on the repo
 			releasedLabels: ['published'] // tag PRs with [published] when release succeeded
 		}]
